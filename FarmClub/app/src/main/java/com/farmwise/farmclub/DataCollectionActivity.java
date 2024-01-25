63e717a2ac175883ad95dd77c058bec5fffbc0fa
Ankit Deb <deb.ankit.work@gmail.com>
2024-01-25 14:36:31 +0530

    setting up project structure

---
+++ FarmClub/app/src/main/java/com/farmwise/farmclub/DataCollectionActivity.java
@@ -0,0 +1,30 @@
+package com.farmwise.farmclub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+// Implement permissions request logic
+// Implement UI logic to collect farmer data
+
+public class DataCollectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_collection);
+
+
+
+
+        // Initialize UI components and set listeners
+        // Request necessary permissions
+        // Implement logic to capture images and videos
+        // Implement basic field validations
+        // Store data in a local database (SQLite)
+
+
+    }
+}
---
+++ FarmClub/app/src/main/java/com/farmwise/farmclub/DisplayDataActivity.java
@@ -0,0 +1,14 @@
+package com.farmwise.farmclub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_data);
+    }
+}
